b50cef9379298b4a0339e46af70cc75d6a2354b7
Jasper Yu <007seadog@gmail.com>
2016-09-22 12:18:06 +0800

    Added PhantomJS Setup

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+uname=`uname -a`
+version='2.1.1'
+[ -n "`echo $uname | grep 'Darwin'`" ] && echo 'OSX' && file='macosx.zip' & system=1
+[ -z "$file" ] && echo Linux && file='linux-x86_64.tar.bz2' && system=2
+
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-$version-$file
+
+[ $system -eq 1 ] && unzip phantomjs-$version-$file
+[ $system -eq 2 ] && tar xf phantomjs-$version-$file
+
+set -- "$file"
+IFS="."; declare -a tmp=($*)
+IFS=""
+path="phantomjs-$version-${tmp[0]}"
+echo $path
+
+mv $path/bin/phantomjs .
+rm -r $path phantomjs-$version-$file
